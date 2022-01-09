944491d9353875acd560fbac8123cf1eda7428f2
tanvir <malam152159@bscse.uiu.ac.bd>
2022-01-09 18:45:51 +0600

    subtraction line added

--- main.java
+++ main.java
@@ -2,9 +2,12 @@ public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
+      int num1 = 5, num2 = 15, sub, sum;
       sum = num1 + num2;
+      sub= num1 -  num2;
 
       System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Sub of these numbers: "+sub);
+
    }
 }
