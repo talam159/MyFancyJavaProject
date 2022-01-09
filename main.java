9ceff504d988e9af919892ebc18c4feecb2c33d7
mhasan <mhasan173013@bscse.uiu.ac.bd>
2022-01-09 12:14:04 +0600

    singleton pattern implemented

--- main.java
+++ main.java
@@ -1,10 +1,46 @@
-public class AddTwoNumbers {
+package creational.pattern;
 
-   public static void main(String[] args) {
-        
-      int num1 = 5, num2 = 15, sum;
-      sum = num1 + num2;
+class DbConnection{
 
-      System.out.println("Sum of these numbers: "+sum);
+    private static DbConnection instance=null;
+
+    private DbConnection(String dbUser,String dbPassword,String dbName ) { 
+    	SQLConnection connection = new SQLConnection(dbUser, dbPassword, dbName);
    }
+
+
+	public static DbConnection getDbConnection(String dbUser,String dbPassword,String dbName) {
+      if (instance== null ) 
+		instance = new DbConnection(dbUser,dbPassword,dbName) ;                           
+
+      return instance;
+    }
+}
+
+class SQLConnection {
+	
+	String dbUser;
+	String dbPassword;
+	String dbName;
+	
+	public SQLConnection(String dbUser, String dbPassword, String dbName) {
+		super();
+		this.dbUser = dbUser;
+		this.dbPassword = dbPassword;
+		this.dbName = dbName;
+	}
+	
+
+}
+
+
+class Test
+{
+	 public static void main(String[] args) throws Exception
+	 {
+		 String dbUser="admin";
+		 String dbPassword="admin";
+		 String dbName="tesDb";
+		 DbConnection connection=DbConnection.getDbConnection(dbUser, dbPassword, dbName);
+	 }
 }
